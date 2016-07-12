2a85639ff83190826d804b2c66bb7f0b9453ba1d
alcivar-inca <eddy_epn2008@hotmail.com>
2016-07-12 16:31:43 -0500

    EPN_AVENTURE 1.1
    Se presenta la estructura con la que se va a realizar el juego..
    Los bloques en los que se debe incluir el codigo correspondiente esta
    comentado

---
+++ EPN_AVENTURE.py
@@ -0,0 +1,40 @@
+#   PRIMERA VERSION EPN AVENTURE
+
+#   importamos la libreria Pygame
+import pygame
+
+#	DECLARACION DE CONSTANTES DE LOS COLORES DE PANTALLA A UTILIZAR
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0 ,0)
+
+
+pygame.init() #	Inicializa el pygame
+
+tamanio=[800, 600] #	Tamaño de la ventana
+
+pantalla = pygame.display.set_mode(tamanio)	#	Creacion de la ventana
+pygame.display.set_caption("Mi primera animacion")	#	Nombre de la ventana
+
+echo = True	# Variable para ingrear al bucle WHILE
+reloj = pygame.time.Clock()	#	Variaable para asignar el tiempo de actualizacion de la pantalla
+
+while echo:
+	#	Bucle principal de los eventos realizados con teclado
+    for evento in pygame.event.get():
+    	#	INICIA GRUPO DE CONDICIONES PARA LOS DIFERENTES EVENTOS
+        if evento.type == pygame.QUIT: #	Si reciona cerrar la ventana cierra el bucle while y sale
+        	echo=False
+       	#	CIERRA TODAS LAS CONDICIONES PARA LOS EVENTOS
+
+    #	INICIA BLOQUE DE LA LOGICA DEL JUEGO
+    #	FINALIZA BLOQUE DE LA LOGICA DEL JUEGO
+
+    
+    #	Inicia bloque que dibuja el juego
+    pantalla.fill(NEGRO)
+    #	Cierra el bloque de dibuja el juego	
+
+	
+	pygame.display.flip()
+    reloj.tick(60)
+pygame.quit()
