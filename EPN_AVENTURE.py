d82848d69f41741849908812adfd6cf0e03bdf72
alcivar-inca <eddy_epn2008@hotmail.com>
2016-07-21 09:04:55 -0500

    Version 1.2
    Se encuentra las imagenes del jugador y fondo del nivel uno

--- EPN_AVENTURE.py
+++ EPN_AVENTURE.py
@@ -1,40 +1,54 @@
 #   PRIMERA VERSION EPN AVENTURE
 
-#   importamos la libreria Pygame
 import pygame
+import time
+pygame.init() 
 
-#	DECLARACION DE CONSTANTES DE LOS COLORES DE PANTALLA A UTILIZAR
-BLANCO = (255, 255, 255)
-NEGRO = (0, 0 ,0)
-
-
-pygame.init() #	Inicializa el pygame
-
-tamanio=[800, 600] #	Tamaño de la ventana
-
+tamanio=[1280, 720] #	Tamanioo de la pantalla a ejecutarse
 pantalla = pygame.display.set_mode(tamanio)	#	Creacion de la ventana
-pygame.display.set_caption("Mi primera animacion")	#	Nombre de la ventana
+pygame.display.set_caption("EPN AVENTURE") #	Titulo de la ventana del juego
+nivel_1 = pygame.image.load("pista_1.jpg") #	 asignamos la imagen a una constante
 
-echo = True	# Variable para ingrear al bucle WHILE
+sentido = pygame.image.load("Der_1.png")
+bala=pygame.image.load("bala1.png")
+dir=1
+
+X=0
+Y=405
+
+echo = True
 reloj = pygame.time.Clock()	#	Variaable para asignar el tiempo de actualizacion de la pantalla
 
 while echo:
+	pantalla.blit(nivel_1, [0,0])
+	pantalla.blit(sentido, [X,Y])
+
 	#	Bucle principal de los eventos realizados con teclado
-    for evento in pygame.event.get():
-    	#	INICIA GRUPO DE CONDICIONES PARA LOS DIFERENTES EVENTOS
-        if evento.type == pygame.QUIT: #	Si reciona cerrar la ventana cierra el bucle while y sale
-        	echo=False
-       	#	CIERRA TODAS LAS CONDICIONES PARA LOS EVENTOS
+	for evento in pygame.event.get():
+		if evento.type == pygame.QUIT: #	Si reciona cerrar la ventana cierra el bucle while y sale
+			echo=False
+		
+		if evento.type==pygame.KEYDOWN:
+			if evento.key==pygame.K_RIGHT: # TECLA DERECHA
+				X +=5
+				sentido=pygame.image.load("Der_1.png")
+				dir=1
+			if evento.key==pygame.K_LEFT: # TECLA IZQUIERDA
+				X -=5
+				sentido=pygame.image.load("Izq_1.png")
+				dir=2
+			if evento.key==pygame.K_UP: #	TECLA HACIA ARRIBA
+				Y -=20
+			if evento.key==pygame.K_DOWN:
+				Y +=20
+			if evento.key== pygame.K_SPACE:
+				if dir ==1:
+					for i in range (X, 1280, 15):
+						pantalla.blit(bala, [90+i, Y+22])
+				else:
+					for i in range (X, 0, -15):
+						pantalla.blit(bala, [i, Y+22])
 
-    #	INICIA BLOQUE DE LA LOGICA DEL JUEGO
-    #	FINALIZA BLOQUE DE LA LOGICA DEL JUEGO
-
-    
-    #	Inicia bloque que dibuja el juego
-    pantalla.fill(NEGRO)
-    #	Cierra el bloque de dibuja el juego	
-
-	
 	pygame.display.flip()
-    reloj.tick(60)
+	reloj.tick(20)
 pygame.quit()
